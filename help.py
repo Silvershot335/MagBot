95457f02a59b6258b1bb410b5ce4768b88ebdd66
Silvershot335 <silvershot335@gmail.com>
2022-03-16 16:12:14 -0500

    Help update

--- help.py
+++ help.py
@@ -21,4 +21,9 @@ async def Help(message):
             value = 'Send your messages to the mods.\nUse this command in the Mod Mail channel.\n-modmail Example Text',
             inline=False
     )
+        embed.add_field(
+            name = '-poll',
+            value = 'Start a poll!\n-poll Option (for 1 option)\n-poll\nOption1\nOption2\n(For Multi-Option)',
+            inline=False
+        )
         await message.channel.send(embed=embed)
