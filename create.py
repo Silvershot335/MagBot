fe54404a378e12e3037ac63e13c3f3c5d4a50500
Silvershot335 <silvershot335@gmail.com>
2022-03-21 12:24:46 -0500

    I Don't Even Fuckin Know Dude

--- create.py
+++ create.py
@@ -10,4 +10,27 @@ def create():
         print(e)
     return conn
 
+def table(conn, sql):
+    cur = conn.cursor()
+    cur.execute(sql)
+
+def main():
+    database = './databases/BotCommands.db'
+
+    sql_create_testing_table = """ CREATE TABLE IF NOT EXISTS BotCommands (
+                                    id integer PRIMARY KEY,
+                                    key text NOT NULL,
+                                    value text NOT NULL
+    );"""
+
+    conn = create(database)
+
+    if conn is not None:
+        table(conn, sql_create_testing_table)
+    else:
+        print('Error! You did the BIG DUMB!')
+
+if  __name__ == '__main__':
+    main()
+
 
